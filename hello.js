5963142a1bdc2e394660d65caeb1c2bf49a3e131
Mr.Steven <vanhungbkcbg1@gmail.com>
2017-01-12 11:25:37 +0700

    event

--- hello.js
+++ hello.js
@@ -1,6 +1,13 @@
 var http = require('http');
+var fs=require('fs');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World!\n');
+    fs.readFile(__dirname+'/read.txt', function (err, data) {
+        if(err){
+            res.end('have an error');
+        }
+
+        res.end(data);
+    });
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
