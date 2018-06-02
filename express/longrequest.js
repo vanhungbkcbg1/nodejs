0e15efc6e1d2ebba4ced56588c2d0b390a4433b8
hungnv <vanhungbkcbg1@gmail.com>
2018-06-02 19:14:30 +0700

    ff

--- express/longrequest.js
+++ express/longrequest.js
@@ -30,7 +30,7 @@ var server = app.listen(8081, function () {
     var port = server.address().port;
 
     console.log("Example app listening at http://%s:%s", host, port)
+    console.log(server.timeout);
 
 });
 
-server.timeout = 240000;
